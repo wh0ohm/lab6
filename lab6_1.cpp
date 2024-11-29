41bddcefbaa3ced29c6316b13da51ea344a400b1
wh0ohm <wararat.boonchieng@gmail.com>
2024-11-29 15:57:17 +0700

    okok

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num[100];
+    int even=0;
+    int odd=0;
+    int i=0;
+    
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> num[i];
+        
+        if(num[i]==0){
+            break;
+        }i++;
+    }
+    
+    for(int j=0;j<i;j++){
+        if(num[j]%2==0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+    
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
